0cbc2409346912ab48590e66fb1a638b434e7401
fenris <xhtmlblog@gmail.com>
2016-01-16 12:34:58 +0500

    first commit of 60 minutes

--- 60minutes/js/main.js
+++ 60minutes/js/main.js
@@ -9,13 +9,9 @@
             utils.burgerBtn();
             utils.bootstrap();
             utils.equalHeight();
-            utils.sliderOnMain();
-            utils.collapseFooterNav();
             utils.phoneMask();
             utils.customFile();
             utils.collapseLinkText();
-            utils.autoPlayYouTubeModal();
-            utils.productCarousel();
             utils.scrollToYear();
         }
     });
@@ -23,12 +19,6 @@
     window.Utils = Class.extend({
         init:function(){},
 
-        sliderOnMain: function(){
-            $('.gallery .bxslider').bxSlider({
-                mode: 'fade',
-                pager: false
-            });
-        },
         bootstrap:function(){
             function isTouchDevice(){
                 return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
@@ -42,26 +32,6 @@
         equalHeight:function(){
             var equalBlock = $('[class^=col-] .block');
             equalBlock.matchHeight();
-
-            var equalBrand = $('.our-brands [class^=col-]');
-            equalBrand.matchHeight();
-
-            var brandblockHeight = equalBrand.height();
-            equalBrand.css('line-height', brandblockHeight + 'px');
-
-            var equalCategory = $('.category-wrapper [class^=col-] .category-item');
-            equalCategory.matchHeight();
-
-            var equalFeature = $('.category-wrapper .feature-wrapper .feature-item');
-            equalFeature.matchHeight();
-
-            var equalShopLlist = $('.shop-list .row > div');
-            equalShopLlist.matchHeight();
-
-            var equalPartners = $('.row .inner');
-            equalPartners.matchHeight();
-            var equalPartnersP = $('.promo p');
-            equalPartnersP.matchHeight();
         },
         validateForm:function(){
 
@@ -73,35 +43,6 @@
                         required : true
                     },
                     tel: {
-                        required : true
-                    }
-                },
-                highlight: function(element, errorClass, showErrors) {
-                    $(element).parent().addClass(errorClass);
-                    $(".error-mess").text('Заполните все поля!');
-                },
-                unhighlight: function(element, errorClass, showErrors) {
-                    $(element).parent().removeClass(errorClass);
-                    $(".error-mess").text('');
-                },
-                errorPlacement: function(error,element) {}
-
-            });
-
-            $('#example form').validate({
-
-                errorClass: "error",
-                rules : {
-                    uname: {
-                        required : true
-                    },
-                    tel: {
-                        required : true
-                    },
-                    addr: {
-                        required : true
-                    },
-                    company: {
                         required : true
                     }
                 },
@@ -134,21 +75,7 @@
                 });
             };
         },
-        collapseFooterNav:function(){
-            var windowWidth = $(window).width();
 
-            //console.log(windowWidth)
-
-            if(windowWidth <= 980) {
-                var collapseLink = $('.footer .nav .caption');
-
-                collapseLink.click(function(e){
-                    e.preventDefault();
-                    $(this).next().slideToggle();
-                    $(this).parent().siblings().find('ul').slideUp();
-                });
-            }
-        },
         phoneMask:function(){
             var phoneInput = $('input[type="tel"]');
             phoneInput.mask('+7 (999) 999-9999');
@@ -164,18 +91,6 @@
                 } else {
                     $(this).text('подробнее о вакансии');
                 }
-            });
-        },
-        autoPlayYouTubeModal:function(){
-            var trigger = $("body").find('[data-toggle="modal"]');
-            trigger.click(function() {
-                var theModal = $(this).data( "target" ),
-                    videoSRC = $(this).attr( "data-theVideo" ),
-                    videoSRCauto = videoSRC+"?autoplay=1" ;
-                $(theModal+' iframe').attr('src', videoSRCauto);
-                $(theModal+' button.close').click(function () {
-                    $(theModal+' iframe').attr('src', videoSRC);
-                });
             });
         },
         customFile:function(){
@@ -199,26 +114,6 @@
                     }
 
                 });
-            });
-        },
-        productCarousel:function(){
-            $('#carousel').flexslider({
-                animation: "slide",
-                controlNav: false,
-                directionNav: false,
-                animationLoop: false,
-                slideshow: false,
-                itemWidth: 120,
-                itemMargin: 25,
-                asNavFor: '#slider'
-            });
-
-            $('#slider').flexslider({
-                animation: "slide",
-                controlNav: false,
-                animationLoop: false,
-                slideshow: false,
-                sync: "#carousel"
             });
         },
         scrollToYear:function(){
