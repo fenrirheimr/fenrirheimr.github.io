68d9d0f342216f0634fcffcaa5436a0232b1a1de
fenris <xhtmlblog@gmail.com>
2016-01-26 09:23:37 +0500

    checkerboard temp

--- 60minutes/js/main.js
+++ 60minutes/js/main.js
@@ -253,6 +253,28 @@
             //
             //});
 
+            /*var $item = $('div.item'), //Cache your DOM selector
+                visible = 2, //Set the number of items that will be visible
+                index = 0, //Starting index
+                endIndex = ( $item.length / visible ) - 1; //End index
+
+            $('div#arrowR').click(function(){
+                if(index < endIndex ){
+                    index++;
+                    $item.animate({'left':'-=300px'});
+                }
+            });
+
+            $('div#arrowL').click(function(){
+                if(index > 0){
+                    index--;
+                    $item.animate({'left':'+=300px'});
+                }
+            });*/
+
+
+            //////////////////////////////////////////////////////////////////////////
+
             var headers = $('.scheduler-header .month-title');
             var monthsHolder = $('.rooms-wrapper .table-wrapper .month-holder');
 
