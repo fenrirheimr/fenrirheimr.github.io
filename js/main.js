98c9d4e231ca1420c69e0ef466edd4bb0a4fde54
Vladimir Glushkov <xhtmlblog@gmail.com>
2016-07-27 11:52:23 +0500

    fenrirheimr / update file structure

--- js/main.js
+++ js/main.js
@@ -1,35 +1,121 @@
+(function($, undefined){
+    'use strict';
 
-$(function() {
-    $('#fullpage').fullpage({
-        //sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-        //anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-        menu: '#menu',
-        afterLoad: function(anchorLink, index){
-
-            //section 2
-            if(index == 2){
-                //moving the image
-                $('#section1').find('img').delay(500).animate({
-                    left: '0%'
-                }, 1500, 'easeOutExpo');
-
-                $('#section1').find('p').first().fadeIn(1800, function(){
-                    $('#section1').find('p').last().fadeIn(1800);
-                });;
-
-            }
-
-            //section 3
-            if(anchorLink == '3rdPage'){
-                //moving the image
-                $('#section2').find('.intro').delay(500).animate({
-                    left: '0%'
-                }, 1500, 'easeOutExpo');
-            }
+    window.App = Class.extend({
+        init: function(){
+            var utils = new window.Utils;
+            utils.bootstrap();
+            utils.customJS();
+            utils.matchHeight();
         }
     });
 
+    window.Utils = Class.extend({
+        init:function(){},
 
+        bootstrap:function(){
+            function isTouchDevice(){
+                return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+            }
+            if(isTouchDevice()===false) {
+                $('[data-toggle="tooltip"]').tooltip();
+            }
+
+        },
+
+        customJS:function(){
+            $( document ).ready(function() {
+
+                scaleVideoContainer();
+
+                initBannerVideoSize('.video-container .poster img');
+                initBannerVideoSize('.video-container .filter');
+                initBannerVideoSize('.video-container video');
+
+                $(window).on('resize', function() {
+                    scaleVideoContainer();
+                    scaleBannerVideoSize('.video-container .poster img');
+                    scaleBannerVideoSize('.video-container .filter');
+                    scaleBannerVideoSize('.video-container video');
+                });
+
+            });
+
+            function scaleVideoContainer() {
+
+                var height = $(window).height();
+                var unitHeight = parseInt(height) + 'px';
+                $('.homepage-hero-module').css('height',unitHeight);
+
+            }
+
+            function initBannerVideoSize(element){
+
+                $(element).each(function(){
+                    $(this).data('height', $(this).height());
+                    $(this).data('width', $(this).width());
+                });
+
+                scaleBannerVideoSize(element);
+
+            }
+
+            function scaleBannerVideoSize(element){
+
+                var windowWidth = $(window).width(),
+                    windowHeight = $(window).height(),
+                    videoWidth,
+                    videoHeight;
+
+                //console.log(windowHeight);
+
+                $(element).each(function(){
+                    var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+                    $(this).width(windowWidth);
+
+                    if(windowWidth < 1000){
+                        videoHeight = windowHeight;
+                        videoWidth = videoHeight / videoAspectRatio;
+                        $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+                        $(this).width(videoWidth).height(videoHeight);
+                    }
+
+                    $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+                });
+            }
+
+            //$('#scene').parallax({
+            //    calibrateX: true,
+            //    calibrateY: true,
+            //    invertX: false,
+            //    invertY: true,
+            //    limitX: false,
+            //    limitY: 10,
+            //    scalarX: 2,
+            //    scalarY: 8,
+            //    frictionX: 0.2,
+            //    frictionY: 0.8,
+            //    originX: 0.0,
+            //    originY: 1.0
+            //});
+
+        },
+        matchHeight:function(){
+            //$('.promo-slider .p-item').matchHeight();
+            //$('.selection-tower-crane .spare-parts-crane .s-item').matchHeight();
+        }
+    });
+
+    $(document).ready(function() {
+        $('body').data('app', new App());
+    });
+
+})(jQuery);
+$(function() {
+    //jQuery is required to run this code
 
 });
 
---
+++ porfolio/Sputnik/js/main.js
@@ -0,0 +1,186 @@
+
+(function($, undefined){
+    'use strict';
+
+    window.App = Class.extend({
+        init: function(){
+            var utils = new window.Utils;
+            utils.bootstrap();
+            utils.customJS();
+            utils.equalHeight();
+            utils.hamburgerBtn();
+            utils.bxSlider();
+            utils.tagCloud();
+        }
+    });
+
+    window.Utils = Class.extend({
+        init:function(){},
+
+        bootstrap:function(){
+            function isTouchDevice(){
+                return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+            }
+            if(isTouchDevice()===false) {
+                $('[data-toggle="tooltip"]').tooltip();
+            }
+
+            $('.header').affix({
+                offset: 30
+            });
+
+            var windowsWidth = $(window).width();
+
+            if(windowsWidth >= 1200) { // то нужно потом ввернуть
+                var pageWrapper = $('.page-wrapper');
+                var footerHeight = $('.footer').outerHeight()-56;
+                // TODO: хз откуда вылазят эти 56 пикселей надо будет найти
+
+                console.log(footerHeight)
+
+                pageWrapper.css('marginBottom', footerHeight);
+            }
+
+        },
+
+        customJS:function(){
+
+            var windowsWidth = $(window).width();
+
+            if(windowsWidth >= 1200) { // то нужно потом ввернуть
+                var pageWrapper = $('.page-wrapper');
+                var footerHeight = $('.footer').outerHeight()-56;
+                // TODO: хз откуда вылазят эти 56 пикселей надо будет найти
+
+                console.log(footerHeight)
+
+                pageWrapper.css('marginBottom', footerHeight);
+            }
+
+            var headerSection = $('.header-wrapper');
+            var videoBlockHeight = headerSection.find('.promo-wrapper').height();
+            headerSection.css('height', videoBlockHeight);
+
+            //$('section').addClass('hidden-block').viewportChecker({
+            //    classToAdd: 'visible animated fadeIn',
+            //    classToRemove:'hidden-block',
+            //    offset: 90
+            //});
+
+            $('.material input, .material textarea').blur(function() {
+                var $this = $(this);
+                if ($this.val())
+                    $this.addClass('used');
+                else
+                    $this.removeClass('used');
+            });
+
+            $('textarea').autosize();
+
+            $('select').selectbox({
+                onOpen: function (inst) {
+                    //firstRow.hide();
+                },
+                onClose: function (inst) {
+                    //firstRow.show();
+                },
+                effect: "slide"
+            });
+
+            var $root = $('html, body');
+            $('.promo-caption .more').click(function() {
+                var href = $.attr(this, 'href');
+                $root.animate({
+                    scrollTop: $(href).offset().top-70
+                }, 500, function () {
+                    window.location.hash = href;
+                });
+                return false;
+            });
+        },
+
+        equalHeight:function(){
+            var equalWhomItem = $('.whom .whom-item');
+            equalWhomItem.matchHeight();
+
+            var equalBlock = $('.blocks .row > div');
+            equalBlock.matchHeight();
+
+            var equalThumbs = $('.thumbnail .caption');
+            equalThumbs.matchHeight();
+
+            var equalTeamThumbs = $('.team .thumbnail > div');
+            equalTeamThumbs.matchHeight();
+
+            var equalBlogCaption = $('.blog-entry .caption');
+            equalBlogCaption.matchHeight();
+
+            var equalBlogText = $('.blog-entry .text');
+            equalBlogText.matchHeight();
+
+            var equalMassMedia = $('.mass-media .row > div');
+            equalMassMedia.matchHeight();
+        },
+        hamburgerBtn:function(){
+
+            var hamburgerBtn = $('.header .hamburger-btn');
+            var nav = $('.header .navigation');
+
+            hamburgerBtn.click(function(){
+                $(this).toggleClass('open');
+                nav.toggle();
+            })
+
+        },
+        bxSlider:function(){
+            $('.slider .slider-wrapper').bxSlider({
+                minSlides: 1,
+                maxSlides: 1,
+                moveSlides: 1,
+                slideMargin: 0,
+                adaptiveHeight: true
+            });
+        },
+        tagCloud:function(){
+            var words = [
+                {text: "Управление", weight: 13, link: 'tag.html'},
+                {text: "Lorem", weight: 13, link: 'tag.html'},
+                {text: "Маркетинг", weight: 9.5, link: 'tag.html'},
+                {text: "Ipsum", weight: 10.5, link: 'tag.html'},
+                {text: "Инструменты", weight: 7.4, link: 'tag.html'},
+                {text: "Dolor", weight: 9.4, link: 'tag.html'},
+                {text: "Менеджмент", weight: 8, link: 'tag.html'},
+                {text: "PR-стратегии", weight: 10, link: 'tag.html'},
+                {text: "Sit", weight: 8, link: 'tag.html'},
+                {text: "Amet", weight: 6.2, link: 'tag.html'},
+                {text: "Consectetur", weight: 5, link: 'tag.html'},
+                {text: "Adipiscing", weight: 5, link: 'tag.html'},
+                {text: "Менеджмент", weight: 5, link: 'tag.html'},
+                {text: "Управление", weight: 13, link: 'tag.html'},
+                {text: "Управление", weight: 13, link: 'tag.html'},
+                {text: "Lorem", weight: 13, link: 'tag.html'},
+                {text: "Маркетинг", weight: 9.5, link: 'tag.html'},
+                {text: "Ipsum", weight: 10.5, link: 'tag.html'},
+                {text: "Инструменты", weight: 7.4, link: 'tag.html'},
+                {text: "Dolor", weight: 9.4, link: 'tag.html'},
+                {text: "Менеджмент", weight: 8, link: 'tag.html'},
+                {text: "PR-стратегии", weight: 10, link: 'tag.html'},
+                {text: "Sit", weight: 8, link: 'tag.html'},
+                {text: "Amet", weight: 6.2, link: 'tag.html'},
+                {text: "Consectetur", weight: 5, link: 'tag.html'},
+                {text: "Adipiscing", weight: 5, link: 'tag.html'},
+                {text: "Менеджмент", weight: 5, link: 'tag.html'},
+                {text: "Управление", weight: 13, link: 'tag.html'}
+            ];
+
+            $('.tag-wrapper').jQCloud(words, {
+                height: 400
+            });
+        }
+    });
+
+    $(document).ready(function() {
+        $('body').data('app', new App());
+    });
+
+})(jQuery);
