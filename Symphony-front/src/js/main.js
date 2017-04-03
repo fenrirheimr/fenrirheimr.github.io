99f91b25f09664fdbd9059635245cbb51af9892a
Vladimir Glushkov <xhtmlblog@gmail.com>
2017-04-03 18:24:24 +0500

    second

--- Symphony-front/src/js/main.js
+++ Symphony-front/src/js/main.js
@@ -1,154 +1,58 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
-//= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js
-//= ../../bower_components/particles.js/particles.js
-
-//= ../../bower_components/fullpage.js/dist/jquery.fullPage.min.js
-
-
+//= ../../bower_components/walkway/walkway.min.js
+//= ../../bower_components/vivus/dist/vivus.min.js
 //= partials/modernizr.custom.js
-//= partials/classie.js
-//= partials/demo1.js
 
-$(function() {
-    $('.burgerBtn').click(function(){
-        $(this).toggleClass('open');
+
+(function($) {
+    "use strict";
+
+    var mobileBtn = $('.nav-toggle');
+    var navMenu = $('.nav-menu');
+
+    mobileBtn.click(function () {
+        $(this).toggleClass('is-active');
+        navMenu.toggleClass('is-active');
     });
 
-    // $('#fullpage').fullpage({
-    //     autoScrolling: false,
-    //     // css3: false
-    //     paddingTop: '100px',
-    //     paddingBottom: '100px'
+    new Vivus('mask', {duration: 50});
+
+    // new Vivus('mask', {}, function (obj) {
+    //     obj.el.classList.add('finished');
     // });
 
-    function showViewPortSize(display) {
-        if(display) {
-            var height = jQuery(window).height();
-            var width = jQuery(window).width();
-            jQuery('body').prepend('<div id="viewportsize" style="z-index:9999;position:fixed;top:40px;left:5px;color:#fff;background:#000;padding:10px">Height: '+height+'<br>Width: '+width+'</div>');
-            jQuery(window).resize(function() {
-                height = jQuery(window).height();
-                width = jQuery(window).width();
-                jQuery('#viewportsize').html('Height: '+height+'<br>Width: '+width);
-            });
-        }
-    }
+    // var svg = new Walkway('#loader');
+    //
+    // svg.draw(function() {
+    //     console.log('Animation finished');
+    // });
 
-    showViewPortSize(true);
-
-    /* ---- particles.js config ---- */
-
-    particlesJS("particles-js", {
-        "particles": {
-            "number": {
-                "value": 120,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#ffffff"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 3,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 150,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 6,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "grab"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 140,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true
-    });
+    // var loaderWrapp = $('#mask');
+    // var letter = loaderWrapp.find('.letter');
+    //
+    // loaderWrapp.each(function () {
+    //     letter.fadeIn();
+    // })
 
 
+
+})(jQuery);
+
+$(function() {
+
+    // function showViewPortSize(display) {
+    //     if(display) {
+    //         var height = jQuery(window).height();
+    //         var width = jQuery(window).width();
+    //         jQuery('body').prepend('<div id="viewportsize" style="z-index:9999;position:fixed;top:40px;left:5px;color:#fff;background:#000;padding:10px">Height: '+height+'<br>Width: '+width+'</div>');
+    //         jQuery(window).resize(function() {
+    //             height = jQuery(window).height();
+    //             width = jQuery(window).width();
+    //             jQuery('#viewportsize').html('Height: '+height+'<br>Width: '+width);
+    //         });
+    //     }
+    // }
+    //
+    // showViewPortSize(true);
 
 });
